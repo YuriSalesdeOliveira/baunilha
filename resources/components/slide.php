b296b3878be0952a057a868d9d891a8343973e71
Yuri Sales de Oliveira <oliveira@pop-os.localdomain>
2022-11-11 23:29:36 -0300

    Composer: add illuminate/database

--- resources/components/slide.php
+++ resources/components/slide.php
@@ -1,7 +1,7 @@
 <script>
 
     let slides = document.querySelectorAll('.slide');
-    console.log(slides);
+
     slides.forEach((slide) => {
         slide.addEventListener('click', () => {
             slide.classList.add('slide__azure');
